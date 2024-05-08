943c481d2eec1d4706595731edd8a99ffaff1c39
arsbakh <arsbakh@gmail.com>
2024-05-08 12:43:43 +0300

    firs commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void _min() {
+        assertEquals(1,Main._min(Main.read("numbers1.txt")));
+
+    }
+
+    @Test
+    void _max() {
+    }
+
+    @Test
+    void _sum() {
+    }
+
+    @Test
+    void _mult() {
+    }
+}
